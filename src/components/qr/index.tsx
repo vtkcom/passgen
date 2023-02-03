26ce7209193fd30e52c046dd9f8d4e6799da5b9a
Boris Hasikov <hasikov@laapl.ru>
2023-02-03 23:19:52 +0300

    Fix

--- src/components/qr/index.tsx
+++ src/components/qr/index.tsx
@@ -15,7 +15,7 @@ const qrCode = new QRCodeStyling({
   backgroundOptions: {
     color: "transparent",
   },
-  image: "/public/tonkeeper.svg",
+  image: "/passgen/tonkeeper.svg",
   imageOptions: {
     margin: 3,
     hideBackgroundDots: true,
