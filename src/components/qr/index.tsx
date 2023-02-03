0d5bd3d2c89f63e8ac235a43f10300d27f15e756
Boris Hasikov <hasikov@laapl.ru>
2023-02-03 23:01:52 +0300

    Styled qr

--- src/components/qr/index.tsx
+++ src/components/qr/index.tsx
@@ -1,11 +1,65 @@
-import { QRCodeSVG } from "qrcode.react";
+import QRCodeStyling from "qr-code-styling";
+import { useEffect, useRef } from "react";
+import WebApp from "@twa-dev/sdk";
+import style from "./index.module.css";
+
+const qrCode = new QRCodeStyling({
+  width: 250,
+  height: 250,
+  margin: 0,
+  type: "canvas",
+  qrOptions: {
+    errorCorrectionLevel: "Q",
+  },
+  backgroundOptions: {
+    color: "transparent",
+  },
+  image: "/tonkeeper.svg",
+  imageOptions: {
+    margin: 3,
+    hideBackgroundDots: true,
+    imageSize: 0.4,
+  },
+  dotsOptions: {
+    type: "extra-rounded",
+    color: "#999",
+    gradient: {
+      type: "linear",
+      rotation: 45,
+      colorStops: [
+        { offset: 0, color: "#45aef5" },
+        { offset: 1, color: "#666" },
+      ],
+    },
+  },
+  cornersSquareOptions: {
+    type: "extra-rounded",
+    color: "#45aef5",
+  },
+  cornersDotOptions: {
+    type: "dot",
+    color: "#45aef5",
+  },
+});
 
 interface Props {
-  text: string;
+  url: string;
 }
 
-const Component: React.FC<Props> = ({ text }) => {
-  return <QRCodeSVG value={text} />;
+const Component: React.FC<Props> = ({ url }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    qrCode.append(ref?.current ?? undefined);
+  }, []);
+
+  useEffect(() => {
+    qrCode.update({
+      data: url,
+    });
+  }, [url]);
+
+  return <div className={style.qr} ref={ref} />;
 };
 
 export default Component;
--- src/routes/connect/index.tsx
+++ src/routes/connect/index.tsx
@@ -1,5 +1,7 @@
+import Qr from "../../components/qr";
+
 const Component: React.FC = () => {
-  return null;
+  return <Qr url="https://fragment.com/59ed5647-4096-4888-9556-261a2e4b4930" />;
 };
 
 export default Component;
