8f40b80c411521f01c45267640c38c7892971d6a
Boris Hasikov <hasikov@laapl.ru>
2023-02-04 10:42:16 +0300

    Change routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {
   RouterProvider,
-  HashRouter,
-  createHashRouter,
+  createBrowserRouter,
   createRoutesFromElements,
   Route,
-  Routes,
 } from "react-router-dom";
 import TonConnectProvider from "./components/tonconnectprovider";
 import "./index.css";
@@ -16,18 +14,23 @@ import Home from "./routes/home";
 import About from "./routes/about";
 import Connect from "./routes/connect";
 
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="connect" element={<Connect />} />
+    </Route>
+  ),
+  {
+    basename: "/passgen",
+  }
+);
+
 ReactDOM.createRoot(document.querySelector("app") as HTMLElement).render(
   <React.StrictMode>
     <TonConnectProvider manifestUrl="https://vtkcom.github.io/passgen/tonconnect-manifest.json">
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="connect" element={<Connect />} />
-          </Route>
-        </Routes>
-      </HashRouter>
+      <RouterProvider router={router} />
     </TonConnectProvider>
   </React.StrictMode>
 );
