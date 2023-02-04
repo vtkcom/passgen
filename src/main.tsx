d9d5183d9dd0f82f066abd4b5f3e20f85ae84ca5
Boris Hasikov <hasikov@laapl.ru>
2023-02-04 20:49:10 +0300

    Перенес роутинг

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,16 @@
 import "@twa-dev/sdk";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  RouterProvider,
-  HashRouter,
-  createBrowserRouter,
-  createHashRouter,
-  createRoutesFromElements,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { StoreContext } from "storeon/react";
-import Layout from "./routes/layout";
-import Home from "./routes/home";
-import About from "./routes/about";
-import Connect from "./routes/connect";
-import "./index.css";
+import { router } from "./routes";
 import { store } from "./store";
-
-export const router = createHashRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="connect" element={<Connect />} />
-    </Route>
-  ),
-  {
-    basename: "/passgen",
-  }
-);
+import "./index.css";
 
 ReactDOM.createRoot(document.querySelector("app") as HTMLElement).render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="connect" element={<Connect />} />
-          </Route>
-        </Routes>
-      </HashRouter>
+      <RouterProvider router={router} />
     </StoreContext.Provider>
   </React.StrictMode>
 );
---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import About from "./about";
+import Connect from "./connect";
+import Home from "./home";
+import Layout from "./layout";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="connect" element={<Connect />} />
+    </Route>
+  ),
+  {
+    basename: "/passgen",
+  }
+);
