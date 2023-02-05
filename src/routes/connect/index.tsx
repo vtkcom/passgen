6b95ca8d15be77f31916d5c5feeedb0be02928f9
Boris Hasikov <hasikov@laapl.ru>
2023-02-05 19:03:48 +0300

    Редирект со страницы коннекта

--- src/routes/connect/index.tsx
+++ src/routes/connect/index.tsx
@@ -1,5 +1,6 @@
 import WebApp from "@twa-dev/sdk";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useStoreon } from "storeon/react";
 import Button from "../../components/button";
 import Qr from "../../components/qr";
@@ -8,27 +9,40 @@ import style from "./index.module.css";
 
 const Component: React.FC = () => {
   const { profile, dispatch } = useStoreon<State, Event>("profile");
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (
-      profile.wallet === null &&
-      !profile.connect.isLoading &&
-      profile.connect.data === null &&
-      profile.wallets.data.length
-    ) {
-      console.log("pre", profile.wallets.data);
+  useEffect(preConnect, [profile.wallets, profile.wallet]);
+  useEffect(afterConnect, [profile.wallet]);
 
+  function preConnect() {
+    const isNotConnect = profile.wallet === null;
+    const isNotLoadingConnect = !profile.connect.isLoading;
+    const isEmptyConnectURL = profile.connect.data === null;
+    const isNotEmptyWallets = profile.wallets.data.length;
+    const isNeedNewURL =
+      isNotConnect &&
+      isNotLoadingConnect &&
+      isEmptyConnectURL &&
+      isNotEmptyWallets;
+
+    if (isNeedNewURL) {
       dispatch("profile/preconnect", { wallet: profile.wallets.data[0] });
     }
-  }, [profile.wallets, profile.wallet]);
+  }
 
   function buttonConnect() {
     WebApp.openLink(profile.connect.data!);
   }
 
+  function afterConnect() {
+    if (profile.wallet)
+      navigate(location.state?.openEndpoint ?? "/", { replace: true });
+  }
+
   return (
     <div className={style.connect}>
-      {profile.wallet === null && profile.connect.data !== "" && (
+      {profile.wallet === null && (
         <>
           <Qr url={profile.connect.data!} />
           <Button isToncoin onClick={buttonConnect}>
