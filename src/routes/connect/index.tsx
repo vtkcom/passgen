e8d40c6e45e8738af57781e394e2d2a1f1e83ea5
Boris Hasikov <hasikov@laapl.ru>
2023-02-05 22:00:19 +0300

    Check

--- src/routes/connect/index.tsx
+++ src/routes/connect/index.tsx
@@ -1,8 +1,6 @@
-import WebApp from "@twa-dev/sdk";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useStoreon } from "storeon/react";
-import Button from "../../components/button";
 import Qr from "../../components/qr";
 import Title from "../../components/title";
 import Wrap from "../../components/wrap";
@@ -10,18 +8,21 @@ import { Event, State } from "../../store";
 import style from "./index.module.css";
 
 const Component: React.FC = () => {
-  const { profile, dispatch } = useStoreon<State, Event>("profile");
+  const {
+    profile: { wallet, wallets, connect },
+    dispatch,
+  } = useStoreon<State, Event>("profile");
   const location = useLocation();
   const navigate = useNavigate();
 
-  useEffect(preConnect, [profile.wallets, profile.wallet, profile.connect]);
-  useEffect(afterConnect, [profile.wallet]);
+  useEffect(preConnect, [wallets, wallet, connect]);
+  useEffect(afterConnect, [wallet]);
 
   function preConnect() {
-    const isNotConnect = profile.wallet === null;
-    const isNotLoadingConnect = !profile.connect.isLoading;
-    const isEmptyConnectURL = profile.connect.data === "";
-    const isNotEmptyWallets = profile.wallets.data.length;
+    const isNotConnect = wallet === null;
+    const isNotLoadingConnect = !connect.isLoading;
+    const isEmptyConnectURL = connect.data === "";
+    const isNotEmptyWallets = wallets.data.length;
     const isNeedNewURL =
       isNotConnect &&
       isNotLoadingConnect &&
@@ -29,21 +30,21 @@ const Component: React.FC = () => {
       isNotEmptyWallets;
 
     if (isNeedNewURL) {
-      dispatch("profile/preconnect", { wallet: profile.wallets.data[0] });
+      dispatch("profile/preconnect", { wallet: wallets.data[0] });
     }
   }
 
   function afterConnect() {
-    if (profile.wallet)
+    if (wallet)
       navigate(location.state?.openEndpoint ?? "/", { replace: true });
   }
 
   return (
     <Wrap style={{ gridTemplateRows: "max-content auto" }}>
       <Title>Connect to Tonkeeper</Title>
-      {profile.wallet === null && (
+      {connect.data !== null && connect.data !== "" && (
         <div className={style.connect}>
-          <Qr url={profile.connect.data!} />
+          <Qr url={connect.data} />
           <p>
             Corporis asperiores est ut perspiciatis. Distinctio provident
             cupiditate doloribus error sunt aspernatur dolores. Fugiat sapiente
