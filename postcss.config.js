27d31af0e22b8564f5489df320e1ff2c6cbe49c5
Boris Hasikov <hasikov@laapl.ru>
2023-02-06 03:34:40 +0300

    Настроил postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,15 @@
-import autoprefixer from "autoprefixer";
-
 export default {
-  plugins: [autoprefixer],
+  plugins: {
+    /**
+     * @type {import("postcss-preset-env").pluginOptions}
+     */
+    "postcss-preset-env": {
+      /* use stage 3 features + css nesting rules */
+      stage: 4,
+      features: {
+        "nesting-rules": true,
+      },
+    },
+    "postcss-color-mod-function": {},
+  },
 };
