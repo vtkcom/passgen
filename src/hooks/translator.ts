bc4c1c24578e8b895293b4711c2849f9ecd1742a
Boris Hasikov <hasikov@laapl.ru>
2023-02-12 14:18:10 +0300

    Добавил несколько переводов

---
+++ src/hooks/translator.ts
@@ -0,0 +1,13 @@
+import { Scope, TranslateOptions } from "i18n-js";
+import { useCallback, useMemo } from "react";
+import { i18n } from "../translation/i18n";
+
+export function useTranslator() {
+  const i18nMemo = useMemo(() => i18n, []);
+
+  const t = useCallback((scope: Scope, options?: TranslateOptions) => {
+    return i18nMemo.t(scope, options);
+  }, []);
+
+  return t;
+}
